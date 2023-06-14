cadd2eba36637894318e771eb1359f871ac82ae2
qyubee <quentinbaron.qb@gmail.com>
2023-06-14 20:44:07 +0300

    fix: build

--- pages/createnewchart.js
+++ pages/createnewchart.js
@@ -1,5 +1,4 @@
-import  Header  from './static/header';
-import  ErrorPage  from './errorcreatingchart';
+import Header from './static/header';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
 
@@ -9,14 +8,80 @@ export default function CreateChartPage() {
 
     if (status === 'loading') return <h1> loading... please wait</h1>;
 
+    const style = ` /* Popup container */
+    .popup {
+        position: relative;
+    display: inline-block;
+    cursor: pointer;
+    }
+
+    /* The actual popup (appears on top) */
+    .popup .popuptext {
+        visibility: hidden;
+    width: 160px;
+    background-color: #555;
+    color: #fff;
+    text-align: center;
+    border-radius: 6px;
+    padding: 8px 0;
+    position: absolute;
+    z-index: 1;
+    bottom: 125%;
+    left: 50%;
+    margin-left: -80px;
+    }
+
+    /* Popup arrow */
+    .popup .popuptext::after {
+        content: "";
+    position: absolute;
+    top: 100%;
+    left: 50%;
+    margin-left: -5px;
+    border-width: 5px;
+    border-style: solid;
+    border-color: #555 transparent transparent transparent;
+    }
+
+    /* Toggle this class when clicking on the popup container (hide and show the popup) */
+    .popup .show {
+        visibility: visible;
+    -webkit-animation: fadeIn 1s;
+    animation: fadeIn 1s
+    }
+
+    /* Add animation (fade in the popup) */
+    @-webkit-keyframes fadeIn {
+        from {opacity: 0;}
+    to {opacity: 1;}
+    }
+
+    @keyframes fadeIn {
+        from {opacity: 0;}
+    to {opacity:1 ;}
+    } `;
+
+
+    // One liner function:
+    const addCSS = css => document.head.appendChild(document.createElement("style")).innerHTML = css;
+
+    // Usage: 
+    addCSS(style)
+
     return (
         <div>
             <Header></Header>
-            <ErrorPage></ErrorPage>
+            <script>
+
+            </script>
+            <div class="popup" onclick="myFunction()">Click me!
+                <span class="popuptext" id="myPopup">ERROR</span>
+            </div>
+
             <button onClick={() => { router.push('/mycharts'); }}>cancel</button>
             <button onClick={() => {
                 //traitement
-                const uploadable=false;
+                const uploadable = false;
                 if (uploadable) {
                     router.push('/newchartdone');
                 } else {
@@ -24,8 +89,8 @@ export default function CreateChartPage() {
                     popup.classList.toggle("show");
 
                     // router.push('/errorcreatingchart');
-                } 
-                 }}>new chart</button>
+                }
+            }}>new chart</button>
         </div>
     );
 
--- pages/newchartdone.js
+++ pages/newchartdone.js
@@ -1,1 +1,26 @@
 import  Header  from './static/header';
+import { useSession, signOut } from 'next-auth/react';
+import { useRouter } from 'next/router';
+
+export default function NewChartDonePage() {
+    const { data,status } = useSession();
+    const router = useRouter();
+
+    if (status === 'loading') return <h1> loading... please wait</h1>;
+    if (data === null) return router.push('/');;
+
+    return (
+        <div>
+            <Header></Header>
+            <p>{data.user.email}</p>
+            <button onClick={() => { 
+                //Insert db
+                router.push('/mycharts');
+                 }}>save to my chart</button>            
+
+            <button onClick={() => { router.push('/createnewchart'); }}>discard</button>
+        </div>
+    );
+
+
+}
