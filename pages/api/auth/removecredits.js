240ddee822d5505ced2ee9ef78285ccbba6df782
qyubee <quentinbaron.qb@gmail.com>
2023-07-06 16:56:14 +0200

    feat(user): added credits management

---
+++ pages/api/auth/removecredits.js
@@ -0,0 +1,20 @@
+import { getUser } from '@/lib/db';
+
+export default async function handler(req, res) {
+    if (req.method === 'POST') {
+        // Process a POST request
+        const user = await getUser(req.body.email);
+        
+        if (user instanceof Error) {
+            console.error("Une erreur s'est produite :", user);
+            res.status(500).end(user.toString());
+        } else{
+            user.removeCredit(req.body.credits)    
+            res.status(200).end(user.toString());
+        }
+
+    } else {
+        // Handle any other HTTP method
+        res.redirect(307, '/')
+    }
+}
--- pages/credits.js
+++ pages/credits.js
@@ -1,36 +1,83 @@
 import Header from './static/header';
-import AccountPage from './account'
-import { useSession, signIn, signOut } from 'next-auth/react';
+import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
+import { getUser } from '@/lib/db';
+import { authOptions } from 'pages/api/auth/[...nextauth]'
+import { getServerSession } from "next-auth/next"
+import { UserDB } from '@/lib/db_model';
 
-export default function CreditPage() {
-    const requestDB = [];
-    const { data, status } = useSession();
+export async function getServerSideProps(context) {
+    const session = await getServerSession(context.req, context.res, authOptions)
+
+    if (!session) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false,
+            },
+        }
+    }
+
+    const userDB = await getUser(session.user.email)
+
+    return {
+        props: {
+            userDB: JSON.stringify(userDB),
+        },
+    }
+}
+
+
+export default function CreditPage({ userDB }) {
+    let { data, status } = useSession();
     const router = useRouter();
-
+    let Page;
+    userDB = JSON.parse(userDB)
+    
     if (status === 'loading') return <h1> loading... please wait</h1>;
 
     const ButtonCredit = (props) => {
-        
-        return(
+        return (
             <button onClick={() => {
                 //Insert into database
+                fetch('/api/auth/addcredits', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({email:data.user.email, credits:props.number}),
+                }).then(function(response) {
+                    // console.log(response.status); // Will show you the status
+                    if (response.status!=200 && response.status!=500) {
+                        console.error("HTTP status " + response.status);
+                    }
+                    else{
+                        router.push("/account")
+                    }
 
-                router.push('/account')
+                    // return response.json();
+                })
             }}>{props.number} credits</button>
+        )
+    }
+
+    if (userDB != null) {
+        Page = (
+            <div>
+                <ButtonCredit number={5} />
+                <ButtonCredit number={10} />
+                <ButtonCredit number={20} />
+                <ButtonCredit number={50} />
+            </div>
         )
     }
 
     return (
         <div>
             <Header></Header>
-            <p>This is the first time you are logging with {data.user.email}</p>
-            <ButtonCredit number={5} />
-            <ButtonCredit number={10} />
-            <ButtonCredit number={20} />
-            <ButtonCredit number={50} />
+            <h1>Buy Credits</h1>
+            {status === "authenticated" && Page}
         </div>
     );
 
 }
-
