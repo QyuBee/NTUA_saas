77dbcd5e0657153de607a534004a50090e529da9
qyubee <quentinbaron.qb@gmail.com>
2023-07-07 13:28:23 +0200

    feat(ui): added mantine

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
-import {SessionProvider} from 'next-auth/react'
+import { SessionProvider } from 'next-auth/react'
+import { MantineProvider } from '@mantine/core';
+
 function App({ Component, pageProps }) {
-return (
-<SessionProvider session={pageProps.session}>
-<Component {...pageProps} />
-</SessionProvider>
-);
+    return (
+        <SessionProvider session={pageProps.session}>
+            <MantineProvider withGlobalStyles withNormalizeCSS>
+                <Component {...pageProps} />
+            </MantineProvider>
+        </SessionProvider>
+    );
 }
 export default App;
 
